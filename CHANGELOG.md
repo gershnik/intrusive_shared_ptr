12cd34e2ef40ef0fa7a0e5a71b14edf026fb6367
Eugene Gershnik <gershnik@hotmail.com>
2023-07-22 21:35:36 -0700

    Updated CMake configuration to modernize it and allow local installation

---
+++ CHANGELOG.md
@@ -0,0 +1,38 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+
+## Unreleased
+
+### Changed
+- Updated CMake configuration to modernize it and allow local installation
+
+## [1.3] - 2023-03-31
+
+### Added
+- Added single threaded mode support to `ref_counted`
+
+### Changed
+- Updated documentation
+
+## [1.2] - 2023-03-17
+
+### Added
+- Pre-defined specialization for Python object and type pointer for use in Python extensions. See python_ptr.h header for details.
+
+
+## [1.1] - 2022-06-09
+
+### Changed
+- CMake configuration modified to mark this library sources as PRIVATE rather than INTERFACE. This plays nicer with IDEs and avoid polluting library clients with its headers
+
+## [1.0] - 2022-05-22
+
+### Added
+- First release
+
+[1.0]: https://github.com/gershnik/intrusive_shared_ptr/releases/v1.0
+[1.1]: https://github.com/gershnik/intrusive_shared_ptr/releases/v1.1
+[1.2]: https://github.com/gershnik/intrusive_shared_ptr/releases/v1.2
+[1.3]: https://github.com/gershnik/intrusive_shared_ptr/releases/v1.3
---
+++ cmake/install.cmake
@@ -0,0 +1,49 @@
+#
+# Copyright 2004 Eugene Gershnik
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://github.com/gershnik/intrusive_shared_ptr/blob/master/LICENSE.txt
+#
+
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+install(TARGETS isptr EXPORT isptr FILE_SET HEADERS DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+install(EXPORT isptr NAMESPACE isptr:: FILE isptr-exports.cmake DESTINATION ${CMAKE_INSTALL_LIBDIR}/isptr)
+
+configure_package_config_file(
+        ${CMAKE_CURRENT_LIST_DIR}/isptr-config.cmake.in
+        ${CMAKE_CURRENT_BINARY_DIR}/isptr-config.cmake
+    INSTALL_DESTINATION
+        ${CMAKE_INSTALL_LIBDIR}/isptr
+)
+
+write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/isptr-config-version.cmake
+    COMPATIBILITY SameMajorVersion
+    ARCH_INDEPENDENT
+)
+
+install(
+    FILES
+        ${CMAKE_CURRENT_BINARY_DIR}/isptr-config.cmake
+        ${CMAKE_CURRENT_BINARY_DIR}/isptr-config-version.cmake
+    DESTINATION
+        ${CMAKE_INSTALL_LIBDIR}/isptr
+)
+
+file(RELATIVE_PATH FROM_PCFILEDIR_TO_PREFIX ${CMAKE_INSTALL_FULL_DATAROOTDIR}/isptr ${CMAKE_INSTALL_PREFIX})
+string(REGEX REPLACE "/+$" "" FROM_PCFILEDIR_TO_PREFIX "${FROM_PCFILEDIR_TO_PREFIX}") 
+
+configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/isptr.pc.in
+    ${CMAKE_CURRENT_BINARY_DIR}/isptr.pc
+    @ONLY
+)
+
+install(
+    FILES
+        ${CMAKE_CURRENT_BINARY_DIR}/isptr.pc
+    DESTINATION
+        ${CMAKE_INSTALL_DATAROOTDIR}/pkgconfig
+)
