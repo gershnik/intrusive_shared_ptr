10f46d43431bf34781ace02ce740fb0690c91a12
Eugene Gershnik <gershnik@users.noreply.github.com>
2024-08-13 22:10:51 -0700

    Fixing python tests crashing on newer CPython releases

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
+#if ISPTR_USE_PYTHON
+    #include <Python.h>
+#endif
+
 int main(int argc, char** argv)
 {
-    return Catch::Session().run( argc, argv );
+#if ISPTR_USE_PYTHON
+    Py_Initialize();
+#endif
+    int ret = Catch::Session().run( argc, argv );
+#if ISPTR_USE_PYTHON
+    if (Py_FinalizeEx() < 0) {
+        return 120;
+    }
+#endif
+    return ret;
 }
