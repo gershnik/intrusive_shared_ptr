e98cb744543d4c4e1e4e30fd99742c3fe16d15fd
Eugene Gershnik <gershnik@users.noreply.github.com>
2026-06-11 17:24:51 -0700

    Relocating both CMake and meson install lib->share

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -10,16 +10,20 @@ include(GNUInstallDirs)
 include(CMakePackageConfigHelpers)
 
 install(TARGETS isptr EXPORT isptr FILE_SET HEADERS DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-install(EXPORT isptr NAMESPACE isptr:: FILE isptr-exports.cmake DESTINATION ${CMAKE_INSTALL_LIBDIR}/isptr)
+install(EXPORT isptr NAMESPACE isptr:: FILE isptr-exports.cmake DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/isptr)
 
-install(FILES ${SRCDIR}/modules/isptr.cppm DESTINATION ${CMAKE_INSTALL_LIBDIR}/isptr)
+install(FILES ${SRCDIR}/modules/isptr.cppm DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/isptr)
+
+# Clean the old CMAKE_INSTALL_LIBDIR location where things were previously put
+# on older versions
+install(CODE "file(REMOVE_RECURSE \"\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/isptr\")")
 
 
 configure_package_config_file(
         ${CMAKE_CURRENT_LIST_DIR}/isptr-config.cmake.in
         ${CMAKE_CURRENT_BINARY_DIR}/isptr-config.cmake
     INSTALL_DESTINATION
-        ${CMAKE_INSTALL_LIBDIR}/isptr
+        ${CMAKE_INSTALL_DATAROOTDIR}/isptr
 )
 
 write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/isptr-config-version.cmake
@@ -32,7 +36,7 @@ install(
         ${CMAKE_CURRENT_BINARY_DIR}/isptr-config.cmake
         ${CMAKE_CURRENT_BINARY_DIR}/isptr-config-version.cmake
     DESTINATION
-        ${CMAKE_INSTALL_LIBDIR}/isptr
+        ${CMAKE_INSTALL_DATAROOTDIR}/isptr
 )
 
 file(RELATIVE_PATH FROM_PCFILEDIR_TO_PREFIX ${CMAKE_INSTALL_FULL_DATAROOTDIR}/isptr ${CMAKE_INSTALL_PREFIX})
